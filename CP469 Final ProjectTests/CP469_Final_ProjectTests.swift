424506d33015646475d8928a5c11ea3b741a6f5f
Ryan Mak <makx1280@p--cs-mini-539.wlu.ca>
2018-04-03 18:13:09 -0400

    Initial Commit

---
+++ CP469 Final ProjectTests/CP469_Final_ProjectTests.swift
@@ -0,0 +1,36 @@
+//
+//  CP469_Final_ProjectTests.swift
+//  CP469 Final ProjectTests
+//
+//  Created by Ryan Mak on 2018-04-03.
+//  Copyright © 2018 Mak & Wu Collaborative. All rights reserved.
+//
+
+import XCTest
+@testable import CP469_Final_Project
+
+class CP469_Final_ProjectTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
